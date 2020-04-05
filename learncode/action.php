2fdd2b1a24675ede4f3cb76ad5149d6571c485d4
cwt2030 <cwt2030@gmail.com>
2020-04-05 19:38:42 +0300

    for

--- learncode/action.php
+++ learncode/action.php
@@ -1,7 +1,26 @@
 <!-- cheking User submitted data using the $_POST superglobal-->
 <!-- create using the file action.php and add the following cod it -->
 
+
 <?php
+	  $servername ="localhost:3306";
+      $username ="ippdf";
+	  $password ="oraX@2030";
+	  $dbname = "admin_ippdf";
+	  //الاتصال بقاعدة البيانات 
+      $conn = new mysqli($servername, $username, $password, $dbname);
+      $col =$_POST["name1"];
+      $Rest =$_POST["Phone"];
+      $Pass =$_POST["Password"]
+
+  ?>
+
+
+
+
+
+<?php
+/*
 $user ="username";
 $pass ="password";
 if (isset($_POST["username"]))
@@ -13,5 +32,15 @@ if (isset($_POST["password"]))
     $pass=$_POST["password"]
 }
 each "Dear $user, your password is " $pass";
-?>
+ 
+*/
 
+
+
+
+
+$sql ="INSERT INTO userdata (Name, Phone, Pass ) VALUES ('$col' ,'$Rest' ,'$Pass')";
+mysqli_query($conn,$sql);
+
+ ?>
+ 
